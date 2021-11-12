47bec5fc293a2234495b982649c5a408aedba7df
Bryce Wein <bryce.wein@gmail.com>
2021-11-11 22:10:40 -0500

    Just setting up

---
+++ script.js
@@ -0,0 +1,38 @@
+const net = new brain.NeuralNetwork()
+
+const data = [
+  {
+    input: { r: 0, g: 0, b: 0 },
+    output: [1]
+  },
+  {
+    input: { r: 1, g: 1, b: 1 },
+    output: [0]
+  }
+]
+
+
+net.train(data)
+
+
+const colorEl = document.getElementById('color')
+const guessEl = document.getElementById('guess')
+const whiteButton = document.getElementById('white-button')
+const blackButton = document.getElementById('black-button')
+const printButton = document.getElementById('print-button')
+
+let color
+
+setRandomColor()
+
+function setRandomColor() {
+  color = {
+    r: Math.random(),
+    g: Math.random(),
+    b: Math.random()
+  }
+  const guess = net.run(color)[0]
+  guessEl.style.color= guess > .5 ? '#FFF' : '#000'
+  colorEl.style.backgroundColor = 
+    `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255})`
+}
